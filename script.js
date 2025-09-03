0a6bb52da66691aaff8351f9efc05ed8d50fd02a
Mohamedkav <151113054+Mohamedkav@users.noreply.github.com>
2025-09-03 12:08:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for nav links
+document.querySelectorAll("nav ul li a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
